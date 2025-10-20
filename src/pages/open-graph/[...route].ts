efd968aadc13ae948143aa2f22676cc80c7143c8
Víctor Peña Romero <victor.pena@minu.mx>
2025-10-19 19:06:57 -0600

    Draft carta a mi perro

--- src/pages/open-graph/[...route].ts
+++ src/pages/open-graph/[...route].ts
@@ -32,18 +32,17 @@ export const { getStaticPaths, GET } = OGImageRoute({
         color: [28, 28, 28],
         size: 68,
         weight: 'SemiBold',
-        families: ['PingFang SC']
+        families: ['Urbanist']
       },
       description: {
         color: [180, 180, 180],
         size: 40,
         weight: 'Medium',
-        families: ['PingFang SC']
+        families: ['Urbanist']
       }
     },
     fonts: [
-      'https://cdn.jsdelivr.net/npm/font-pingfang-sc-font-weight-improved@latest/PingFangSC-Medium.woff2',
-      'https://cdn.jsdelivr.net/npm/font-pingfang-sc-font-weight-improved@latest/PingFangSC-Semibold.woff2',
+      'https://fonts.bunny.net/css?family=urbanist:400,500,600,700'
     ]
   })
 })
